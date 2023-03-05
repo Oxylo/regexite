067c6756d81c0f1b4c01557ac6badaeeb597bd96
Pieter Marres <pmarres@quantsense.io>
2023-03-05 20:44:22 +0100

    Added Documentation and Man page for strsplit1()

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,3 +1,15 @@
+#' Split a string
+#'
+#' @param x String or Vector containing 1 string element
+#' @param split String specifying what to split on
+#'
+#' @return A character vector
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' strsplit1(x, split = ",")
+#' ## [1] "alfa"    "bravo"   "charlie" "delta"
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
